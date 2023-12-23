c68e4c0642e4d6551acb70127cd600eb91aece3d
yoyt777 <yoyt777@gmail.com>
2023-12-23 17:59:33 +0900

    ㅇㅇ

--- project02/src/test/java/com/example/demo/lombok/Quiz4.java
+++ project02/src/test/java/com/example/demo/lombok/Quiz4.java
@@ -17,20 +17,21 @@ import org.springframework.boot.test.context.SpringBootTest;
 public class Quiz4 {
 
 	@Test
-	void test() {		
+	void test() {
 		Movie movie = new Movie();
 		movie.setTitle("서울의 봄");
 		movie.setDirector("김성수");
-		movie.setDate(LocalDate.of(2023, 11, 22));
+		movie.setDate(LocalDate.of(2023, 11, 22)); // 시간 생성
 		System.out.println(movie.getTitle());
 		System.out.println(movie.getDirector());
 		System.out.println(movie.getDate());
-		
-		Movie movie2 = new Movie("3일의 휴가", "육상효 ", LocalDate.of(2023, 12, 6));
+
+		// 개봉일 미정
+		Movie movie2 = new Movie("3일의 휴가", "육상효", null);
 		System.out.println(movie2);
-		
+
 		// builder는 필요한 정보만 입력할 수 있음
-		Movie movie3 = Movie.builder().title("리틀 포레스트").build();
+		Movie movie3 = Movie.builder().title("리틀 포레스트").director("임순례").build();
 		System.out.println(movie3);
 	}
 }
