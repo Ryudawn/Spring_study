fad6ddf126237baeec777f1d9742444b478ab478
yoyt777 <yoyt777@gmail.com>
2024-01-15 18:49:13 +0900

    DDd

--- project07/src/main/java/com/example/demo/entity/Board.java
+++ project07/src/main/java/com/example/demo/entity/Board.java
@@ -15,15 +15,15 @@ public class Board extends BaseEntity { // 등록일, 수정일 필드 상속받
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int no; //글번호
+    int no; //글번호
 
     @Column(length = 100, nullable = false)
-    private String title; //제목
+    String title; //제목
 
     @Column(length = 1500, nullable = false)
-    private String content; //내용
+    String content; //내용
 
     @Column(length = 50, nullable = false)
-    private String writer; //작성자
+    String writer; //작성자
 
 }
